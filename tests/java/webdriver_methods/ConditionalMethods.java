549d4aa1819c1e794be5d8f498f70af40495ff09
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-08 22:31:59 +0500

    Conditonal Methods

--- tests/java/webdriver_methods/ConditionalMethods.java
+++ tests/java/webdriver_methods/ConditionalMethods.java
@@ -1,10 +1,14 @@
 package webdriver_methods;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 
 public class ConditionalMethods {
+
+    public static ChromeOptions options;
+    public static WebDriver driver;
 
     @BeforeTest
     void Setup() throws InterruptedException {
@@ -15,5 +19,5 @@ public class ConditionalMethods {
         Thread.sleep(1000);
     }
 
-    
+
 }
