2faa5ac533d75b0a18c2a7b7c8af0ccdaeef4f6a
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-09 21:43:41 +0500

    Conditonal Methods

--- tests/java/webdriver_methods/ConditionalMethods.java
+++ tests/java/webdriver_methods/ConditionalMethods.java
@@ -10,7 +10,14 @@ public class ConditionalMethods {
     public static ChromeOptions options;
     public static WebDriver driver;
 
-
+    @BeforeTest
+    void Setup() throws InterruptedException {
+        options = new ChromeOptions();
+        options.addArguments("--remote-allow-origin=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+    }
 
 
 }
