00ad1b8092150cbaffe730c686967e9e3c64a06e
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-11 16:17:02 +0500

    Browser Methods

--- tests/java/webdriver_methods/BrowserMethods.java
+++ tests/java/webdriver_methods/BrowserMethods.java
@@ -1,9 +1,11 @@
 package webdriver_methods;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class BrowserMethods {
 
@@ -19,5 +21,20 @@ public class BrowserMethods {
         Thread.sleep(1000);
     }
 
+    /*
+
+    Browser methods :
+    1. driver.close() - close single browser window
+    2. driver.quite() - close all browser windows
+
+     */
+
+    @Test
+    void browserTest() throws InterruptedException {
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.findElement(By.cssSelector(".wikipedia-icon")).click();
+        Thread.sleep(3000);
+        driver.quit();
+    }
 
 }
