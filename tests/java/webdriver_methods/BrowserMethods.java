5c89dece3f73e76ee14e0c573fa93def446ce172
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-11 11:11:35 +0500

    Browser Methods

---
+++ tests/java/webdriver_methods/BrowserMethods.java
@@ -0,0 +1,23 @@
+package webdriver_methods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+
+public class BrowserMethods {
+
+    public static ChromeOptions options;
+    public static WebDriver driver;
+
+    @BeforeTest
+    void Setup() throws InterruptedException {
+        options = new ChromeOptions();
+        options.addArguments("--remote-allow-origin=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+    }
+
+
+}
