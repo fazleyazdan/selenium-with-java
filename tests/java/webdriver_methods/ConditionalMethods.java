b2a9d769c5e85d9d39138165c420c8f95f3f99ba
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-11 11:41:08 +0500

    Conditional Methods

--- tests/java/webdriver_methods/ConditionalMethods.java
+++ tests/java/webdriver_methods/ConditionalMethods.java
@@ -1,6 +1,8 @@
 package webdriver_methods;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
@@ -24,10 +26,24 @@ public class ConditionalMethods {
     // isDisplayed()
     // isEnabled()
     // isSelected()
+
     @Test
     void condtionsTest()
     {
         driver.get("https://testautomationpractice.blogspot.com/");
 
+        // check is wikipedia logo is displayed or not.
+        // we can do it in two ways, first store element & then do manipulation on it
+
+        WebElement wiki_logo = driver.findElement(By.cssSelector(".wikipedia-icon"));
+        System.out.println("display status of the logo is:" + wiki_logo.isDisplayed());
+
+        // OR we can chain conditional command and do it one statement
+
+        boolean logo_status = driver.findElement(By.cssSelector(".wikipedia-icon")).isDisplayed();
+        System.out.println("display status of the logo is:" + logo_status);
+
+
     }
+
 }
