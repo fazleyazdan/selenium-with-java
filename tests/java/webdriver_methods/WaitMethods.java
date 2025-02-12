1783b5f6da39a2abf2fee3b48aa0c03c2a4e37b8
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-12 18:34:17 +0500

    Wait Methods

--- tests/java/webdriver_methods/WaitMethods.java
+++ tests/java/webdriver_methods/WaitMethods.java
@@ -1,4 +1,22 @@
 package webdriver_methods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+
 public class WaitMethods {
+
+    public static ChromeOptions options;
+    public static WebDriver driver;
+
+    @BeforeTest
+    void Setup() throws InterruptedException {
+        options = new ChromeOptions();
+        options.addArguments("--remote-allow-origin=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+    }
+
 }
