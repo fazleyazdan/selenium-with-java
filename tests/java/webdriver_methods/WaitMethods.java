e499a288f452b899f47a778e30ec1ca2a328bb0f
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-18 22:11:33 +0500

    Wait Methods

--- tests/java/webdriver_methods/WaitMethods.java
+++ tests/java/webdriver_methods/WaitMethods.java
@@ -6,6 +6,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+/*
+
+-   Suppose there are 7 lines of code, and in 3rd line you are accessing an element
+    which is not loaded fully on the web page, in that case you will get an exception because of the
+    Synchronization. it is because the speed of our script is faster than the application response.
+
+-   Now the speed of the application is not in our hand but, we can slow down our script to match
+    the application response.
+
+-   Note : Thread.sleep() is not a webdriver method, it is default method in Java
+-   implicit wait, explicit wait/fluent wait are the only 2 webdriver methods in selenium
+
+ */
 public class WaitMethods {
 
     public static ChromeOptions options;
