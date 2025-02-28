344e689ebee32a7d752c1c2450e073bfa3cb1859
fazleyazdan <fazleyazdan345@gmail.com>
2025-02-28 16:45:10 +0500

    Fluent Wait

--- tests/java/webdriver_methods/FluentWait.java
+++ tests/java/webdriver_methods/FluentWait.java
@@ -1,4 +1,10 @@
 package webdriver_methods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class FluentWait {
+
+    public static ChromeOptions options;
+    public static WebDriver driver;
 }
